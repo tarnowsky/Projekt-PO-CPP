5352861ed9e9bf140519d286c946707cb37356f3
Michal-Tarnowski <michal.tarnowski03@gmail.com>
2023-04-15 12:22:40 +0200

    Barszcz Sosnowskiego added

--- world_simulation/main.cpp
+++ world_simulation/main.cpp
@@ -6,49 +6,25 @@ int main() {
     World w(25, 100);
     w.ShowConsoleCursor(false);
     for (int i = 0; i < 2; i++) {
-        Organism* obj = new Antylopa({ 10,i }, &w);
+        Organism* obj = new BarszczSosnowskiego({ 10,i }, &w);
         w.addOrganism(obj);
     }
 
-    for (int i = 0; i < 2; i++) {
-        Organism* obj = new Wilk({ 20,i }, &w);
-        w.addOrganism(obj);
-    }
-
-    for (int i = 0; i < 2; i++) {
-        Organism* obj = new Zolw({ 30,i }, &w);
-        w.addOrganism(obj);
-    }
-
-    for (int i = 0; i < 2; i++) {
-        Organism* obj = new Lis({ 40,i }, &w);
-        w.addOrganism(obj);
-    }
-
-    for (int i = 0; i < 2; i++) {
-        Organism* obj = new Owca({ 50,i }, &w);
-        w.addOrganism(obj);
-    }
-    
-    for (int i = 0; i < 1; i++) {
-        Organism* obj = new Guarana({ 76,i }, &w);
+    for (int i = 0; i < 15; i++) {
+        Organism* obj = new Antylopa({ 20,i }, &w);
         w.addOrganism(obj);
     }
 
     for (int i = 0; i < 1; i++) {
-        Organism* obj = new Trawa({ 80,i }, &w);
+        Organism* obj = new Guarana({ 30,i }, &w);
         w.addOrganism(obj);
     }
-    
-    for (int i = 0; i < 1; i++) {
-        Organism* obj = new WilczeJagody({ 66,i }, &w);
-        w.addOrganism(obj);
-    }
+
 
     w.drawWorld();
 
     while (true) {
-        cin.get();
+        //cin.get();
         w.nextTurn();
     }
 
